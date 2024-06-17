e4a47910f0a23db4e99398533f4fe011381c0748
Franco Canavese <fcanavese@hotmail.com>
2024-06-17 16:34:06 +0200

    4to commit

---
+++ src/logic.js
@@ -0,0 +1,25 @@
+//Funciones de logica para llevar los puntos:
+
+//Función que recibo los puntos ingresados y evelua se ejecuta
+export const sumarPuntos = (jugadores, turno, puntosDelInput) => {
+  if (jugadores[turno].puntos < 10000) {
+    jugador.puntos = jugador.puntos + puntosDelInput;
+    if (jugadores[turno].puntos === 10000) {
+      return jugadores[turno].apodo;
+    }
+    if (jugadores[turno].puntos > 10000) {
+      jugadores[turno].puntos = jugadore[turno].puntos - puntosDelInput
+      return false
+    };
+  };
+}
+
+// Devuelve apodo del ganador o ''
+export const checkWinner = (jugadores, turno, puntosDelInput) => {
+  const nombreGanador = sumarPuntos(jugadores, turno, puntosDelInput);
+  if (nombreGanador != false) {
+    return nombreGanador;
+  } else {
+    return '';
+  }
+}
